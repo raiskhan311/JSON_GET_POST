58a59b066f770fa8165fb282d076ed56e4a94f52
mac <mac@macs-iMac.local>
2017-07-11 16:42:45 +0530

    simple GET and POST using JSON

--- JSONSerializationDemo/ViewController.swift
+++ JSONSerializationDemo/ViewController.swift
@@ -12,14 +12,70 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btn_GET(_ sender: Any) {
+        let urlstring = "https://jsonplaceholder.typicode.com/users"
+        guard let url = URL(string: urlstring) else  {return}
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let response = response{
+                print("RESPONSE = \(response)")
+            }
+            if let data = data{
+                print("DATA = \(data)")
+                do
+                {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print("JSON = \(json)")
+                }
+                catch
+                {
+                    print(error)
+                }
+                
+            }
+        }.resume()        
+    }
+    
+    @IBAction func btn_POST(_ sender: Any) {
+        let parameter = ["username":"@kilo_loco","tweet":"HelloWorld"]
+        
+        let urlstring = "https://jsonplaceholder.typicode.com/posts"
+        guard let url = URL(string: urlstring) else  {return}
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameter, options: []) else {return}
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response{
+                print("RESPONSE = \(response)")
+            }
+            if let data = data{
+                print("DATA = \(data)")
+                do
+                {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print("JSON = \(json)")
+                }
+                catch
+                {
+                    print(error)
+                }
+                
+            }
+        }.resume()
+    }
 }
 
